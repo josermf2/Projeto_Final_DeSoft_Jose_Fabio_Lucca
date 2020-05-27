275579811fb9f3d8a619f6f3a7fa0cf0ac287659
unknown <josermf2@al.insper.edu.br>
2020-05-27 18:24:16 -0300

    .

--- Primeiros_testes.py
+++ Primeiros_testes.py
@@ -1,4 +1,4 @@
-"""Busãozinho mto loko"""
+"""Foxxer"""
 #Importando bibliotecas
 import os
 import sys
@@ -16,19 +16,40 @@ VERDE = (0, 255, 0)
 AZUL = (0, 0, 255)
 
 #Icone e nome
-icone = pygame.image.load(os.path.join("Imagens", "Busão.png"))
+icone = pygame.image.load(os.path.join("Imagens", "logo_raposa_(1).png"))
 pygame.display.set_icon(icone) 
-pygame.display.set_caption("Busãozinho mto loko")
+pygame.display.set_caption("Foxxer")
 
 #Criando Tela
 tela = pygame.display.set_mode((1200, 700))
 
-#Criando cenarios
-cenario1 = pygame.image.load(os.path.join("Imagens", "Cenario_1.png"))
+#Criando telas
+telainicial = pygame.image.load(os.path.join("Imagens", "Fundo_tela_inicial.png"))
+#cenario1 = pygame.image.load(os.path.join("Imagens", "Cenario_1.png"))
 cenario2 = pygame.image.load(os.path.join("Imagens", "Cenario_2.png"))
 cenario3 = pygame.image.load(os.path.join("Imagens", "Cenario_3.png"))
 cenario4 = pygame.image.load(os.path.join("Imagens", "Cenario_4.png"))
 cenario5 = pygame.image.load(os.path.join("Imagens", "Cenario_5.png"))
+
+"""Classes"""
+"""
+class Raposa():
+    
+    def __init__(self):
+        self.raposa = pygame.image.load(os.path.join("Imagens", "Raposa_1.png"))
+
+class Skate():
+
+class RacingCar(): 
+
+class Busao():
+
+class Caminhao():
+
+class Car1():
+
+class Car2():
+""" 
 
 """Game Loop"""
 #Loop para rodar o jogo
@@ -37,7 +58,7 @@ rodando = True
 while rodando:
     
     tela.fill(PRETO) #preenchendo tela
-    tela.blit(cenario1, (0, 0))
+    tela.blit(telainicial, (0, 0))
 
     #Eventos do jogo
     for evento in pygame.event.get():
