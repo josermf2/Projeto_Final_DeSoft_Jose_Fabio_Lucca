2c91cd720c1b7f85295113bf2a980ba338ab9872
luccabvs <luccabvs@al.insper.edu.br>
2020-06-06 20:39:27 -0300

    foxer

--- Primeiros_testes.py
+++ Primeiros_testes.py
@@ -4,7 +4,7 @@ import os
 import sys
 import pygame
 import random
-import time
+
 
 pygame.init() #Iniciando Pygame
 
@@ -57,27 +57,58 @@ class Car2():
 
 """Testes objetos"""
 lista1 = ['RacingCar.png', 'Busão.png']
-lista2 = [True,True]
+lista2 = [True,False]
 class Rua:
     def __init__(self,posicao):
         self.tem_ou_nao = random.choice(lista2)
         self.posicao_x = posicao[0]
         self.posicao_y = posicao[1]
 
-class Sprite():
-    def __init__(self):
-        self.automovel = random.choice(lista1)
+class Automoveis():
+    def __init__(self,c):
+        self.posicaox = c[0]
+        self.posicaoy = c[1]
+    def sobe_carro(self):
+        self.posicaoy -=10
+    def desce_carro(self):
+        self.posicaoy += 10
 
-objeto = Sprite()
-rua1 = pygame.image.load(os.path.join("Imagens",objeto.automovel)).convert_alpha()
-rua2 = pygame.image.load(os.path.join("Imagens",objeto.automovel)).convert_alpha()
 
-yrua1 = 540
-
-yrua2 = 0
+car1img = pygame.image.load(os.path.join("Imagens",'Car1.png')).convert_alpha()
+busaoimg = pygame.image.load(os.path.join("Imagens",'Busão.png')).convert_alpha()
+racingcarimg = pygame.image.load(os.path.join("Imagens",'RacingCar.png')).convert_alpha()
 
 
 relogio = pygame.time.Clock()
+i1 = [944,540]
+i2 = [816,0]
+i3 = [560,540]
+i4 = [432, 0]
+i5 = [304, 540]
+i6 = [176, 0]
+
+car1_pronto = Automoveis(i1)
+busao1_pronto = Automoveis(i1)
+racingcar1_pronto = Automoveis(i1)
+car2_pronto = Automoveis(i2)
+busao2_pronto = Automoveis(i2)
+racingcar2_pronto = Automoveis(i2)
+car3_pronto = Automoveis(i3)
+busao3_pronto = Automoveis(i3)
+racingcar3_pronto = Automoveis(i3)
+car4_pronto = Automoveis(i4)
+busao4_pronto = Automoveis(i4)
+racingcar4_pronto = Automoveis(i4)
+car5_pronto = Automoveis(i5)
+busao5_pronto = Automoveis(i5)
+racingcar5_pronto = Automoveis(i5)
+car6_pronto = Automoveis(i6)
+busao6_pronto = Automoveis(i6)
+racingcar6_pronto = Automoveis(i6)
+
+lista0 = [car1_pronto,busao1_pronto,racingcar1_pronto]
+lista8 = [car3_pronto,busao3_pronto,racingcar3_pronto]
+
 
 """Game Loop"""
 #Loop para rodar o jogo
@@ -85,25 +116,48 @@ rodando = True
 
 while rodando:
     
-    deltat = relogio.tick(60)
+    deltat = relogio.tick(30)
+    print(deltat)
     tela.fill(PRETO)
     tela.blit(cenario1, (0,0))
 
-
+    
 
     #Eventos do jogo
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT or (evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE): 
             rodando = False
             sys.exit()
-
-    xrua1 = 944
-    yrua1 -= 10
-    tela.blit(rua1, (xrua1, yrua1))
+    d = random.choice(lista0)
+    if d == car1_pronto:
+        tela.blit(car1img, (d.posicaox,d.posicaoy))
+        d.sobe_carro()
+        
     
-    xrua2 = 816
-    yrua2 += 10
-    tela.blit(rua2, (xrua2, yrua2))
+    if d == busao1_pronto:
+        tela.blit(busaoimg, (d.posicaox,d.posicaoy))
+        d.sobe_carro()
+    
+    else:
+        tela.blit(racingcarimg, (d.posicaox,d.posicaoy))
+        d.sobe_carro()
+
+    g = random.choice(lista8)
+    if g == car3_pronto:
+        tela.blit(car1img, (g.posicaox,g.posicaoy))
+        g.sobe_carro()
+    
+    if g == busao3_pronto:
+        tela.blit(busaoimg, (g.posicaox,g.posicaoy))
+        g.sobe_carro()
+    
+    else:
+        tela.blit(racingcarimg, (g.posicaox,g.posicaoy))
+        g.sobe_carro()
+    
+    #xrua2 = 816
+    #yrua2 += 10
+    #tela.blit(rua2, (xrua2, yrua2))
     
 
     pygame.display.update() #atualizando a tela
