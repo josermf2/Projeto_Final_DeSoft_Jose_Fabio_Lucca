bd7a903c377f9502eabceb33091ee3bf1d96c986
Fabio Dotoli <fabio.dotoli02@gmail.com>
2020-06-15 18:43:28 -0300

    Atualiando o arquivo

--- Foxer.py
+++ Foxer.py
@@ -8,6 +8,7 @@ import random
 import math
 import numpy as np
 from classes import Raposa, Automoveis, Frutas, Rua1, Rua2, Rua3, Rua4, Rua5, Rua6 
+from funcoes import colisao, abacaxi_sound, colisao_sound, enter_sound
 
 #Iniciando Pygame
 pygame.init() 
@@ -60,38 +61,6 @@ abacaxiimg = pygame.image.load(os.path.join('Imagens','Abacaxi64.png')).convert_
 vidaimg1 = pygame.image.load(os.path.join('Imagens','vida.png')).convert_alpha()
 vidaimg2 = pygame.image.load(os.path.join('Imagens','vida2.png')).convert_alpha()
 vidaimg3 = pygame.image.load(os.path.join('Imagens','vida3.png')).convert_alpha()
-
-'''Funções'''
-#Criando a colisão do abacaxi
-def colisao(lista1,lista2):
-    distancia = math.sqrt((lista1[0]-lista2[0])**2 + (lista1[1]-lista2[1])**2)
-
-    if distancia <= 74:
-        return True
-    else:
-        return False
-
-#Criando música de fundo
-mixer.init()
-mixer.music.load(os.path.join('Musicas e Efeitos Sonoros','musica_de_fundo.mp3'))
-mixer.music.set_volume(0.2)
-mixer.music.play(-1)
-
-#Criando sons
-def abacaxi_sound():
-    mixer.init()
-    abacaxi = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','abacaxi.ogg'))
-    abacaxi.play()
-
-def colisao_sound():
-    mixer.init()
-    colisao = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','colisao.ogg'))
-    colisao.play()
-
-def enter_sound():
-    mixer.init()
-    enter = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','enter.ogg'))
-    enter.play()
 
 '''Listas'''
 #Criando a lista de velocidades 
---
+++ funcoes.py
@@ -0,0 +1,34 @@
+import pygame 
+from pygame import mixer
+
+'''Funções'''
+#Criando a colisão do abacaxi
+def colisao(lista1,lista2):
+    distancia = math.sqrt((lista1[0]-lista2[0])**2 + (lista1[1]-lista2[1])**2)
+
+    if distancia <= 74:
+        return True
+    else:
+        return False
+
+#Criando música de fundo
+mixer.init()
+mixer.music.load(os.path.join('Musicas e Efeitos Sonoros','musica_de_fundo.mp3'))
+mixer.music.set_volume(0.2)
+mixer.music.play(-1)
+
+#Criando sons
+def abacaxi_sound():
+    mixer.init()
+    abacaxi = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','abacaxi.ogg'))
+    abacaxi.play()
+
+def colisao_sound():
+    mixer.init()
+    colisao = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','colisao.ogg'))
+    colisao.play()
+
+def enter_sound():
+    mixer.init()
+    enter = mixer.Sound(os.path.join('Musicas e Efeitos Sonoros','enter.ogg'))
+    enter.play()
